195ce57aa2eea96dbcd2ee4ff775695af0337456
Fernando Rodriguez <frodriguez.developer@outlook.com>
2014-11-24 17:29:52 -0500

    Added music sync script

---
+++ xidle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <X11/extensions/scrnsaver.h>
+
+
+int main() 
+{
+  //printf("Here ");
+  XScreenSaverInfo *info = XScreenSaverAllocInfo();
+  //printf("Here ");
+  Display *display = XOpenDisplay("acer/unix:0");
+  if (display == NULL)
+  {
+    fprintf((void*)2, "Could not open display :0!");
+    return 1;
+  }
+
+  //printf("Here...");
+ 
+  XScreenSaverQueryInfo(display, DefaultRootWindow(display), info);
+  printf("%u\n", (unsigned int) (info->idle / 1000));
+  
+  XCloseDisplay(display);
+  
+  return 0;
+}
