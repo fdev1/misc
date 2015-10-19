688fdc23b9e657bd3384a80a3b1e4748d2491eba
Fernando Rodriguez <frodriguez.developer@outlook.com>
2015-10-19 17:08:48 -0400

    Updated android sdk permissions script

--- android_sdk_perms.sh
+++ android_sdk_perms.sh
@@ -5,34 +5,38 @@
 # set ownership of all files and directories to root:android
 # and the permissions of all executables (including scripts)
 # to 775, for other files to 664, and for directories to
-# 775.
+# 6775.
 
 
 chmod_file()
 {
-	if [ "$0" == "./fix_permissions.sh" ]; then
-		return
-	fi
 	chown root:android "$0"
 	if [ "$(file "$0" | grep 'ELF\|shell\|Python')" != "" ]; then
-		chmod 775 "$0"
-		echo "$0: 775"
+		chmod -v 775 "$0"
 	else
-		chmod 664 "$0"
-		echo "$0: 664"
+		chmod -v 664 "$0"
 	fi
 }
 
 chmod_directory()
 {
 	chown root:android "$0"
-	chmod 775 "$0"
-	echo "$0: 775"
+	chmod -v 6775 "$0"
+}
+
+fix_perms()
+{
+	find "$0" -type f -exec /bin/sh -c chmod_file '{}' \;
+	find "$0" -type d -exec /bin/sh -c chmod_directory '{}' \;
 }
 
 export -f chmod_file
 export -f chmod_directory
+export -f fix_perms
 
-find . -type f -exec /bin/sh -c chmod_file '{}' \;
-find . -type d -exec /bin/sh -c chmod_directory '{}' \;
+if [ "$0" == "--full" ]; then
+	fix_perms .
+else
+	find . \! -uid 0 -exec /bin/sh -c fix_perms '{}' \;
+fi
 
